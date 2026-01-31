d6ee7e108aa189bc90eea3a32806ce5b0d1f9cf1
Jonah Barrington <jn445613@dal.ca>
2026-01-31 13:30:58 -0400

    added room_2

--- scripts/world.gd
+++ scripts/world.gd
@@ -1,21 +1,23 @@
 extends Node2D
 
 var room
-
 const FIRST_ROOM = preload("res://scenes/rooms/room_1.tscn")
+const SECOND_ROOM = preload("res://scenes/rooms/room_2.tscn")
 
 func _ready() -> void:
 	room = FIRST_ROOM.instantiate()
 	add_child(room)
 
 func _on_lz_top_character_body_entered(body: Node2D) -> void:
-	remove_child(room)
-	if room:
-		room.queue_free()
-	room = next_room()
+	if body is Player:
+		remove_child(room)
+		if room:
+			room.queue_free()
+		room = next_room()
+		(body as Player).position.y += 200
 	
 
 func next_room() -> Node2D:
-	var next = FIRST_ROOM.instantiate()
+	var next = SECOND_ROOM.instantiate()
 	add_child(next)
 	return next
