95fa4aa32c4dbb4704eb5b40704044065a2ea7db
Jonah Barrington <jn445613@dal.ca>
2026-01-31 17:26:21 -0400

    finished enemy spawner

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -1,15 +1,27 @@
 extends Node2D
 
-var enemy_amount = 3
-var enemy_interval = 1.0
+@export var enemy_amount := 10
+@export var enemy_interval := 2.0
+@export var enemy_type := preload("res://scenes/enemies/floaty_head.tscn")
+
 
 func _ready() -> void:
 	while enemy_amount > 0:
 		enemy_amount -= 1
-		spawn_enemy()
-		await get_tree().create_timer(1.0).timeout
+		spawn_enemy(enemy_type)
+		await get_tree().create_timer(enemy_interval).timeout
+
+
+func spawn_enemy(type) -> Enemy:
+	var enemy = type.instantiate() as Enemy
+	var rng = RandomNumberGenerator.new()
+	# top/bottom vs left/right
+	if rng.randi_range(0,1):
+		enemy.position.y += rng.randi_range(0,1) * 200
+		enemy.position.x += rng.randi_range(0,330)
+	else:
+		enemy.position.x += rng.randi_range(0,1) * 330
+		enemy.position.y += rng.randi_range(0,200)
+	get_parent().add_child.call_deferred(enemy)
+	return enemy
 	
-
-
-func spawn_enemy() -> void:
-	pass
