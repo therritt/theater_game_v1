160e103928b1e57073f9943509f139294717176e
Jonah Barrington <jn445613@dal.ca>
2026-01-31 19:10:29 -0400

    added spawner trigger and walls from spawner

---
+++ scripts/spawner_trigger.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+const ENEMY_SPAWNER = preload("res://scenes/enemy_spawner.tscn")
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		get_parent().add_child(ENEMY_SPAWNER.instantiate())
+		queue_free()
