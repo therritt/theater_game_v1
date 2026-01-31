651e56820bb46f4bd5cf427e6ae9d1a1a5b03a09
Jonah Barrington <jn445613@dal.ca>
2026-01-31 14:52:12 -0400

    added generic rooms and room array

--- scripts/world.gd
+++ scripts/world.gd
@@ -5,28 +5,33 @@ var player
 const PLAYER = preload("res://scenes/player.tscn")
 const FIRST_ROOM = preload("res://scenes/rooms/room_1.tscn")
 const SECOND_ROOM = preload("res://scenes/rooms/room_2.tscn")
+const THIRD_ROOM = preload("res://scenes/rooms/room_3.tscn")
+const FOURTH_ROOM = preload("res://scenes/rooms/room_4.tscn")
+const FIFTH_ROOM = preload("res://scenes/rooms/room_2.tscn")
+var room_array = [[FIRST_ROOM, SECOND_ROOM], [THIRD_ROOM,FOURTH_ROOM]]
+var room_position = [0,0]
 
 func _ready() -> void:
 	player = PLAYER.instantiate() as Player
 	player.position.x += 150
 	player.position.y += 100
 	add_child(player)
-	room = FIRST_ROOM.instantiate()
+	room = room_array[room_position[0]][room_position[1]].instantiate()
 	add_child(room)
+
+func next_room() -> Node2D:
+	var next = room_array[room_position[0]][room_position[1]].instantiate()
+	add_child(next)
+	return next
 
 func _on_lz_top_character_body_entered(body: Node2D) -> void:
 	if body is Player:
 		remove_child(room)
 		if room:
 			room.queue_free()
+		room_position[0] = (room_position[0] - 1 + 2) % 2
 		room = next_room()
 		(body as Player).position.y += 200
-	
-
-func next_room() -> Node2D:
-	var next = SECOND_ROOM.instantiate()
-	add_child(next)
-	return next
 
 
 func _on_lz_bottom_character_body_entered(body: Node2D) -> void:
@@ -34,6 +39,7 @@ func _on_lz_bottom_character_body_entered(body: Node2D) -> void:
 		remove_child(room)
 		if room:
 			room.queue_free()
+		room_position[0] = (room_position[0] + 1 + 2) % 2
 		room = next_room()
 		(body as Player).position.y -= 200
 
@@ -43,8 +49,9 @@ func _on_lz_left_character_body_entered(body: Node2D) -> void:
 		remove_child(room)
 		if room:
 			room.queue_free()
+		room_position[1] = (room_position[1] - 1 + 2) % 2
 		room = next_room()
-		(body as Player).position.x += 350
+		(body as Player).position.x += 330
 
 
 func _on_lz_right_character_body_entered(body: Node2D) -> void:
@@ -52,5 +59,6 @@ func _on_lz_right_character_body_entered(body: Node2D) -> void:
 		remove_child(room)
 		if room:
 			room.queue_free()
+		room_position[1] = (room_position[1] + 1 + 2) % 2
 		room = next_room()
-		(body as Player).position.x -= 350
+		(body as Player).position.x -= 330
