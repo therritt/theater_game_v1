df7d3af11af50f522ad7050ecc580c36a9926601
Jonah Barrington <jn445613@dal.ca>
2026-01-31 19:38:56 -0400

    idk

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -3,14 +3,20 @@ extends Node2D
 @export var enemy_amount := 10
 @export var enemy_interval := 2.0
 @export var enemy_type := preload("res://scenes/enemies/floaty_head.tscn")
-
+var dead_enemies = 0
 
 func _ready() -> void:
-	while enemy_amount > 0:
-		enemy_amount -= 1
-		spawn_enemy(enemy_type)
+	var remaining = enemy_amount
+	while remaining > 0:
+		remaining -= 1
+		var curr_enemy = spawn_enemy(enemy_type)
+		#curr_enemy.died.connect(_on_enemy_death)
 		await get_tree().create_timer(enemy_interval).timeout
 
+func _on_enemy_death(dead_enemy: Enemy):
+	dead_enemies += 1
+	if dead_enemies >= enemy_amount:
+		queue_free()
 
 func spawn_enemy(type) -> Enemy:
 	var enemy = type.instantiate() as Enemy
