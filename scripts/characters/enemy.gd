ce955c477e738ba935603e2d0c7626b6267b110c
therritt <99824733+therritt@users.noreply.github.com>
2026-01-31 15:44:02 -0400

    Added enemy floaty head (#4)

---
+++ scripts/characters/enemy.gd
@@ -0,0 +1,11 @@
+extends Character
+class_name Enemy
+
+@export var chase_range := 200
+@export var stop_range := 16
+
+var target: Player = null
+
+func _ready() -> void:
+	var root_scene = get_tree().get_current_scene()
+	target = root_scene.get_node_or_null("Player")
