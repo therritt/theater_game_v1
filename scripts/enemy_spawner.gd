67287648e6f0709bbb5c09e9601a72e4eeb05b50
jn445613 <jn445613@dal.ca>
2026-01-31 20:48:58 -0400

    Spawner (#6)
    * finished enemy spawner
    
    * added spawner trigger and walls from spawner
    
    * idk
    
    * addes exactly one line of code but it was very useful and worth committing

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -1,15 +1,33 @@
 extends Node2D
 
-var enemy_amount = 3
-var enemy_interval = 1.0
+@export var enemy_amount := 10
+@export var enemy_interval := 2.0
+@export var enemy_type := preload("res://scenes/enemies/floaty_head.tscn")
+var dead_enemies = 0
 
 func _ready() -> void:
-	while enemy_amount > 0:
-		enemy_amount -= 1
-		spawn_enemy()
-		await get_tree().create_timer(1.0).timeout
+	var remaining = enemy_amount
+	while remaining > 0:
+		remaining -= 1
+		var curr_enemy = spawn_enemy(enemy_type)
+		curr_enemy.died.connect(_on_enemy_death)
+		await get_tree().create_timer(enemy_interval).timeout
+
+func _on_enemy_death(dead_enemy: Enemy):
+	dead_enemies += 1
+	if dead_enemies >= enemy_amount:
+		queue_free()
+
+func spawn_enemy(type) -> Enemy:
+	var enemy = type.instantiate() as Enemy
+	var rng = RandomNumberGenerator.new()
+	# top/bottom vs left/right
+	if rng.randi_range(0,1):
+		enemy.position.y += rng.randi_range(0,1) * 200
+		enemy.position.x += rng.randi_range(0,330)
+	else:
+		enemy.position.x += rng.randi_range(0,1) * 330
+		enemy.position.y += rng.randi_range(0,200)
+	get_parent().add_child.call_deferred(enemy)
+	return enemy
 	
-
-
-func spawn_enemy() -> void:
-	pass
