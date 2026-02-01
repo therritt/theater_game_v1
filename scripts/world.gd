cbb4d512d072270a2d84b800a95573701d4af1b6
maeve <maevemil13@gmail.com>
2026-01-31 21:03:25 -0400

    added game over screen and theater room 2

--- scripts/world.gd
+++ scripts/world.gd
@@ -9,6 +9,7 @@ const SECOND_ROOM = preload("res://scenes/rooms/room_2.tscn")
 const THIRD_ROOM = preload("res://scenes/rooms/room_3.tscn")
 const FOURTH_ROOM = preload("res://scenes/rooms/room_4.tscn")
 const FIFTH_ROOM = preload("res://scenes/rooms/room_5.tscn")
+const GAME_OVER = preload("res://scenes/game_over.tscn")
 # room array can be customized
 var room_array = [[FIRST_ROOM, SECOND_ROOM], [THIRD_ROOM,FOURTH_ROOM]]
 var ldy # dimensions of room_array. make sure sub-arrays are consistent with each other pls
@@ -20,6 +21,7 @@ func _ready() -> void:
 	player.position.x += 150
 	player.position.y += 100
 	add_child(player)
+	player.died.connect(game_over)
 	ldy = room_array.size()
 	ldx = room_array[0].size()
 	room = room_array[room_position[0]][room_position[1]].instantiate()
@@ -29,6 +31,11 @@ func next_room() -> Node2D:
 	var next = room_array[room_position[0]][room_position[1]].instantiate()
 	add_child(next)
 	return next
+
+func game_over(dead_player: Character):
+	if dead_player is Player:
+		var game_over_screen = GAME_OVER.instantiate()
+		add_child(game_over_screen)
 
 func _on_lz_top_character_body_entered(body: Node2D) -> void:
 	if body is Player:
