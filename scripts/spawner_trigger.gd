7333302f9e9ffe0cf6f62311fe420a026f7b345b
Jonah Barrington <jn445613@dal.ca>
2026-02-01 12:11:05 -0400

    made spawner triggers customizable per room

--- scripts/spawner_trigger.gd
+++ scripts/spawner_trigger.gd
@@ -1,8 +1,14 @@
 extends Area2D
 
+@export var amount = 2
+@export var interval = 1.0
+
 const ENEMY_SPAWNER = preload("res://scenes/enemy_spawner.tscn")
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
-		get_parent().get_parent().add_child.call_deferred(ENEMY_SPAWNER.instantiate())
+		var spawner = ENEMY_SPAWNER.instantiate()
+		spawner.enemy_amount = amount
+		spawner.enemy_interval = interval
+		get_parent().get_parent().add_child.call_deferred(spawner)
 		queue_free()
